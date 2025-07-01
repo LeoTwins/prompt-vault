6c0cbfe4c58b97d28f01aad22fa43295bdd18fda
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2025-07-01 17:45:09 +0900

    first commit

---
+++ PromptVault/PromptVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptVaultApp.swift
+//  PromptVault
+//
+//  Created by Kazuma Sakaguchi on 2025/07/01.
+//
+
+import SwiftUI
+
+@main
+struct PromptVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromptVaultTests/PromptVaultTests.swift
@@ -0,0 +1,17 @@
+//
+//  PromptVaultTests.swift
+//  PromptVaultTests
+//
+//  Created by Kazuma Sakaguchi on 2025/07/01.
+//
+
+import Testing
+@testable import PromptVault
+
+struct PromptVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
